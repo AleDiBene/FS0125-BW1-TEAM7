563c4d1ab0d877690ae2400d9403834fd1dbd3fa
francescofarris <farro.561@gmail.com>
2025-02-25 15:25:00 +0100

    results-francesco

---
+++ resultPage.js
@@ -0,0 +1,19 @@
+
+  function generateResults(correctAnswers, totalQuestions) {
+  let correctPercentage = (correctAnswers / totalQuestions) * 100;
+  let wrongPercentage = 100 - correctPercentage;
+  
+  document.getElementById("correct-text").innerText =
+    `Correct: ${correctPercentage.toFixed(1)}% (${correctAnswers}/${totalQuestions} questions)`;
+  document.getElementById("wrong-text").innerText =
+    `Wrong: ${wrongPercentage.toFixed(1)}% (${totalQuestions - correctAnswers}/${totalQuestions} questions)`;
+  let circumference = 2 * Math.PI * 40; 
+  let correctCircle = document.getElementById("correct-circle");
+  correctCircle.style.strokeDasharray = circumference;
+  correctCircle.style.strokeDashoffset = circumference * (1 - correctPercentage / 100);
+  
+  let message = correctPercentage > 50 ?
+    "Congratulations! You passed the exam.\nWe’ll send you the certificate in a few minutes. Check your email (including promotions / spam folder)." :
+    "Please try again next time";
+  document.getElementById("result-message").innerText = message;
+}
