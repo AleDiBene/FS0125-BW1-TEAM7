042bcc9eec6addebded2fafb5134679e810a20ee
11292-stella <stella.marucelli@gmail.com>
2025-02-25 10:57:14 +0100

    altri problemi con le stelle

--- js/feedback.js
+++ js/feedback.js
@@ -4,8 +4,14 @@ const fStars = function () {
   const allStars = document.querySelectorAll("#formstar label")
 
   for (let i = 0; i < allStars.length; i++) {
-    allStars[i].addEventListener(`click`, function () {
-      console.log("stella cliccata")
+    allStars[i].addEventListener("click", function () {
+      for (let k = 0; k < allStars.length; k++) {
+        allStars[k].classList.remove("starBlue")
+      }
+
+      for (let j = 0; j <= i; j++) {
+        allStars[j].classList.add("starBlue")
+      }
     })
   }
 }
